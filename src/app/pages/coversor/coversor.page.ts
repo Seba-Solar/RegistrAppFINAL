772803058577eef326b1a957febe0cd004f573d9
Seba-Solar <101576567+Seba-Solar@users.noreply.github.com>
2022-12-14 23:49:46 -0300

    coversorxd

--- src/app/pages/coversor/coversor.page.ts
+++ src/app/pages/coversor/coversor.page.ts
@@ -1,18 +1,104 @@
 import { Component, OnInit } from '@angular/core';
 import { MindicatorService } from 'src/app/services/mindicator.service';
-
+import { AlertController, NavController } from '@ionic/angular';
 @Component({
   selector: 'app-convertor',
-  templateUrl: './covertos.page.html',
-  styleUrls: ['./covertos.page.scss'],
+  templateUrl: './coversor.page.html',
+  styleUrls: ['./coversor.page.scss'],
 })
 export class CoversorPage implements OnInit {
 
   pageTitle = 'Conversor de Monedas';
   isNotHome = true;
   listadoMindicador: any=[];
+  listaMonedas;
+  valorMoneda;
+  valorMoneda2;
+  cantMoneda;
+  tipoMoneda;
+  tipoMoneda2;
+  valorPesos2 = 0;
 
-  constructor(private mindicatorService: MindicatorService) { }
+  constructor(private mindicatorService: MindicatorService,
+    public navController: NavController,
+    private alertController: AlertController,) {
+      this.dolareuro();
+    }
+
+    dolareuro() {
+      this.mindicatorService.getPosts()
+      .then(respuesta => {
+        console.log(respuesta);
+
+        if (this.tipoMoneda === 'dolar' && this.tipoMoneda2 ==='clp'){
+          this.valorMoneda = respuesta.dolar.valor;
+          this.valorPesos2 = this.valorMoneda * this.cantMoneda;
+        }
+        else if (this.tipoMoneda === 'euro' && this.tipoMoneda2 === 'clp') {
+          this.valorMoneda = respuesta.euro.valor;
+          this.valorPesos2 = this.valorMoneda * this.cantMoneda;
+        }
+        else if (this.tipoMoneda === 'clp' && this.tipoMoneda2 === 'euro') {
+          this.valorMoneda = respuesta.euro.valor;
+          this.valorPesos2 = this.cantMoneda / this.valorMoneda;
+        }
+        else if (this.tipoMoneda === 'clp' && this.tipoMoneda2 === 'dolar') {
+          this.valorMoneda = respuesta.dolar.valor;
+          this.valorPesos2 = this.cantMoneda / this.valorMoneda;
+        }
+        else if (this.tipoMoneda === 'euro' && this.tipoMoneda2 === 'dolar') {
+          this.valorMoneda = this.cantMoneda * respuesta.euro.valor;
+          this.valorPesos2 = this.valorMoneda / respuesta.dolar.valor;
+        }
+        else if (this.tipoMoneda === 'dolar' && this.tipoMoneda2 === 'euro') {
+          this.valorMoneda = this.cantMoneda * respuesta.dolar.valor;
+          this.valorPesos2 = this.valorMoneda / respuesta.euro.valor;
+        }
+        else if ((this.tipoMoneda === 'dolar' && this.tipoMoneda2 === 'dolar')||
+                (this.tipoMoneda === 'euro' && this.tipoMoneda2 === 'euro') ||
+                (this.tipoMoneda === 'clp' && this.tipoMoneda2 === 'clp')){
+          this.valorPesos2 = -1;
+          this.mostrarAlertaMonedas();
+        }
+
+      },
+      (error) => {
+        console.error(error);
+      }
+     );
+     };
+
+     async mostrarAlertaMonedas() {
+      const alert = await this.alertController.create({
+        header: 'Monedas Iguales',
+        message : 'No tiene sentido convertir dos monedas iguales',
+        buttons : [
+          {
+            text: 'OK',
+            role: 'cancel',
+            cssClass: 'danger',
+          },
+        ]
+      });
+      await alert.present();
+    };
+
+    async mostrarDatosVacios() {
+      if (this.cantMoneda === undefined ) {
+        const alert = await this.alertController.create({
+          header: 'Faltan Datos por completar',
+          message : 'Debes ingresar un número valido',
+          buttons : [
+            {
+              text: 'OK',
+              role: 'cancel',
+              cssClass: 'danger',
+            },
+          ]
+        });
+        await alert.present();
+      };
+    };
 
   ngOnInit() {
     this.cargarApi();
