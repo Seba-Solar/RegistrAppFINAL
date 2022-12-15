9a6229fc3e49044a88d3b15fc4d1eab4d55ff690
Seba-Solar <101576567+Seba-Solar@users.noreply.github.com>
2022-12-15 03:15:29 -0300

    BUG RESUELTO

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 import { IonicModule } from "@ionic/angular";
 import { FabButtonComponent } from "./fab-button/fab-button.component";
-import { FooterComponent } from "./footer/footer.component";
 import { HeaderComponent } from "./header/header.component";
 
 @NgModule({
     declarations : [
         HeaderComponent,
-        FooterComponent,
         FabButtonComponent
     ],
     imports : [
